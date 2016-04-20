95e4e309d785a85a22e45cce9f0047f684191bde
Erick Mattson <Erick Mattson>
2016-04-20 17:13:30 -0500

    tostring

---
+++ Sweepstakes-Erick/ICustomString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Erick
+{
+    interface ICustomString
+    {
+        string ToString();
+        
+    }
+}
---
+++ Sweepstakes-Erick/SystemString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Erick
+{
+    class SystemString : ICustomString
+    {
+        string StringToManipulate;
+        public SystemString(string inputString)
+        {
+            StringToManipulate = inputString;
+        }
+        public override string ToString()
+        {
+            return StringToManipulate.ToString();
+        }
+    }
+}
