d2aa2359d2c2214301cd82fc9eab96ed3e918094
Erick Mattson <Erick Mattson>
2016-04-24 17:47:22 -0500

    final

--- Sweepstakes-Erick/SystemArrayString.cs
+++ Sweepstakes-Erick/SystemArrayString.cs
@@ -25,65 +25,43 @@ namespace Sweepstakes_Erick
         }
         public void Insert(int startIndex, string stringToInsert)
         {
-            char[] newCharsArray1 = makeNewArrayAt(0, startIndex, manipulatedCharArray);
-            char[] newCharsArray2 = makeNewArrayAt(startIndex, manipulatedCharArray.Length, manipulatedCharArray);
-            char[] insertedCharsArray = stringToInsert.ToCharArray();
-            char[][] arrays = new char[][] { newCharsArray1, insertedCharsArray, newCharsArray2 };
-            addArrays(arrays);
-            manipulatedCharArray = addArrays(arrays);
+            char[] temporaryArray = new char[manipulatedCharArray.Count() + stringToInsert.Count()];
+            int i = 0;
+            int j = 0;
+            for (int TemporaryIndex = 0; TemporaryIndex < manipulatedCharArray.Count() + stringToInsert.Count(); TemporaryIndex++)
+            {
+                if (TemporaryIndex >= startIndex && TemporaryIndex < startIndex + stringToInsert.Count())
+                {
+                    temporaryArray[TemporaryIndex] = stringToInsert.ElementAt(i);
+                    i++;
+                }
+                else
+                {
+                    temporaryArray[TemporaryIndex] = manipulatedCharArray[j];
+                    j++;
+                }
+            }
+            manipulatedCharArray = temporaryArray;
         }
         public void Remove(int startIndex, int numCharsToRemove)
         {
-            int startIndex2 = startIndex + numCharsToRemove + 1;
-            char[] newCharsArray1 = makeNewArrayAt(0, startIndex, manipulatedCharArray);
-            char[] newCharsArray2 = makeNewArrayAt(startIndex2, manipulatedCharArray.Length, manipulatedCharArray);
-            char[][] arrays = new char[][] { newCharsArray1, newCharsArray2 };
-            addArrays(arrays);
-            manipulatedCharArray = addArrays(arrays);
+            char[] ShortenedString = new char[manipulatedCharArray.Count() - numCharsToRemove];
+            int i;
+            for (i = 0; i < startIndex; i++)
+            {
+                ShortenedString[i] = manipulatedCharArray[i];
+            }
+            for (int j = startIndex + numCharsToRemove; j < manipulatedCharArray.Count(); j++)
+            {
+                ShortenedString[i] = manipulatedCharArray[j];
+                i++;
+            }
+            manipulatedCharArray = ShortenedString;
         }
         public int Length()
         {
-            return manipulatedCharArray.Length;
+            int Length = manipulatedCharArray.Count();
+            return Length;
         }
-        public char[] makeNewArrayAt(int startIndex, int endIndex, char[] arrayToTakePortion)
-        {
-            int lengthA = endIndex - startIndex;
-            char[] newCharA = new char[0];
-            if (endIndex > startIndex)
-            {
-                newCharA = new char[lengthA];
-            }
-            for (int i = startIndex; i < endIndex; i++)
-            {
-                newCharA[i - startIndex] = manipulatedCharArray.ElementAt(i);
-            }
-            return newCharA;
-        }
-        public char[] addArrays(char[][] arrays)
-        {
-            char[] finalCharA = new char[FindTotalLength(arrays)];
-            int count = 0;
-            for (int j = 0; j < arrays.Length; j++)
-            {
-                for (int i = 0; i < arrays[j].Length; i++)
-                {
-                    finalCharA[count] = arrays[j].ElementAt(i);
-                    count++;
-                }
-            }
-            return finalCharA;
-        }
-        public int FindTotalLength(char[][] arrays)
-        {
-            int lengthA = 0;
-
-            foreach (char[] array in arrays)
-            {
-                lengthA += array.Length;
-            }
-            return lengthA;
-        }
-
     }
 }
-    
